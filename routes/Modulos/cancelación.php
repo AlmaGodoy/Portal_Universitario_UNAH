e18be068db9d006799a10ff0a75d8ec250979841
AlmaGodoy <alma.godoy@unah.hn>
2026-03-13 16:24:07 -0600

    Mejoras en el proyecto para evitar conflictos de Git

---
+++ routes/Modulos/cancelación.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DocumentoExcepcionalController;
+
+/*
+|--------------------------------------------------------------------------
+| API de Cancelaciones
+|--------------------------------------------------------------------------
+*/
+
+Route::prefix('api/cancelaciones')->group(function () {
+    Route::post('crear', [DocumentoExcepcionalController::class, 'subir']);
+    Route::get('todas', [DocumentoExcepcionalController::class, 'obtenerTodos']);
+    Route::get('detalle/{id}', [DocumentoExcepcionalController::class, 'obtenerCancelacion']);
+    Route::delete('eliminar/{id}', [DocumentoExcepcionalController::class, 'eliminar']);
+    Route::post('guardar-documento', [DocumentoExcepcionalController::class, 'guardarDocumento']);
+    Route::put('actualizar/{id}', [DocumentoExcepcionalController::class, 'actualizar']);
+});
