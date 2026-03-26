86b125979e88ef778ed273f980acd936d4b0880c
AlmaGodoy <alma.godoy@unah.hn>
2026-03-26 10:13:58 -0600

    New modulo login registro
    Co-authored-by: bg14891 <bayronfranciscogodoy@gmail.com>

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -1,11 +1,17 @@
+Model/Usuario.php:
+
 <?php
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Usuario extends Model
+class Usuario extends Authenticatable
 {
+    use HasFactory, Notifiable;
+
     protected $table = 'tbl_usuario';
     protected $primaryKey = 'id_usuario';
     public $timestamps = false;
@@ -15,4 +21,21 @@ class Usuario extends Model
         'contraseña',
         'estado_cuenta',
     ];
+
+    protected $hidden = [
+        'contraseña',
+        'remember_token',
+    ];
+
+    // Relación con la tabla empleados
+    public function empleado()
+    {
+        return $this->hasOne(Empleado::class, 'user_id', 'id_usuario');
+    }
+
+    // Indica a Laravel que use 'contraseña' para el login
+    public function getAuthPassword()
+    {
+        return $this->contraseña;
+    }
 }
