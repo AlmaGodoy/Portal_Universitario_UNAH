5391b63a1991e4e2a13d9fd1c1f541e59fc0539c
AlmaGodoy <alma.godoy@unah.hn>
2026-04-04 18:04:08 -0600

    Update archivo vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { glob } from 'glob';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/js/app.js',
                 'resources/css/app.css',
-                ...glob.sync('resources/css/*.css'),
+                'resources/js/app.js',    
+                'resources/css/**/*.css', 
             ],
             refresh: true,
         }),
