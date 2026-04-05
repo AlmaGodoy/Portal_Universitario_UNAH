3886d9e2c087aa4471c5445110474b5a6c053fa1
AlmaGodoy <alma.godoy@unah.hn>
2026-04-04 18:22:27 -0600

    Update de archivo Vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,30 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fs from 'fs';
+import path from 'path';
+
+const getCssFiles = (dir) => {
+    let results = [];
+    const list = fs.readdirSync(dir);
+    list.forEach((file) => {
+        file = path.join(dir, file);
+        const stat = fs.statSync(file);
+        if (stat && stat.isDirectory()) {
+            results = results.concat(getCssFiles(file));
+        } else if (file.endsWith('.css')) {
+            results.push(file.replace(/\\/g, '/')); 
+        }
+    });
+    return results;
+};
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/js/app.js',
                 'resources/css/app.css',
-                'resources/js/app.js',    
-                'resources/css/**/*.css', 
+                ...getCssFiles('resources/css'),
             ],
             refresh: true,
         }),
